f2feb45d133bb91067d395d31cb8108be57bba30
alfonsosancho <alfonso.sancho@hotmail.com>
2016-01-20 22:30:14 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-var nAplicacion ="appBase";
+var nAplicacion ="ideCHG";
 var urlMapea = "http://mapea-sigc.juntadeandalucia.es";
 var contextos = "wmcfile=http://www.callejerodeandalucia.es/wmc/context_cdau_callejero.xml*Callejero,http://www.callejerodeandalucia.es/wmc/context_cdau_satelite.xml*Satélite";
-var layers = "sv17_gran_comercio";
+var layers = "";
 var urlGeosearch = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sigc/search";
 var extra = "operations=searchstreet";
 /******************* Proyecciones *********************/
